774b454b4f5f4c2be2954cd65569a7ca1df963ff
iyungui <rsdbddml@gmail.com>
2024-08-07 09:24:29 +0900

    Initialize project

---
+++ GreenSitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenSitter
+//
+//  Created by Yungui Lee on 8/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
