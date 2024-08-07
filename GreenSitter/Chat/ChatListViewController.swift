a00adc99278f1c5b0809687bd0217bc012fbd260
Jihye Park <deviloper.park@gmail.com>
2024-08-07 15:24:22 +0900

    feat: 채팅목록이 비었을 때 ChatListViewController UI 작업

--- GreenSitter/Chat/ChatListViewController.swift
+++ GreenSitter/Chat/ChatListViewController.swift
@@ -8,22 +8,105 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
-
+    
+    // container
+    private let container: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 30, y: 285, width: 333, height: 281)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    // 아이콘
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ChatIcon")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    // 홈 이동 버튼
+    private let goToHomeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 128/255, green: 188/255, blue: 86/255, alpha: 1.0)
+        button.setTitle("새싹 돌봄이/보호자\n찾아보기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title3)
+        button.titleLabel?.numberOfLines = 2
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.lineBreakMode = .byWordWrapping
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        
+        // padding
+        var buttonConfiguration = button.configuration
+        buttonConfiguration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        button.configuration = buttonConfiguration
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    // 기본 텍스트
+    private let guideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재 참여 중인 채팅이 없습니다."
+        label.textColor = .secondaryLabel
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        
+        // TODO: - 비로그인 시
+        
+        
+        // MARK: - 로그인 시
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup UI
+    func setupUI() {
+        self.view.backgroundColor = .systemBackground
+        
+        self.title = "나의 채팅"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: <#T##Any?#>, action: <#T##Selector?#>)
+        
+        container.addSubview(iconImageView)
+        container.addSubview(goToHomeButton)
+        container.addSubview(guideLabel)
+        
+        self.view.addSubview(container)
+        
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            container.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            container.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            container.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            
+            iconImageView.topAnchor.constraint(equalTo: container.topAnchor),
+            iconImageView.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            iconImageView.widthAnchor.constraint(equalToConstant: 121),
+            iconImageView.heightAnchor.constraint(equalToConstant: 121),
+            
+            goToHomeButton.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 20),
+            goToHomeButton.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            goToHomeButton.widthAnchor.constraint(equalToConstant: 231),
+            goToHomeButton.heightAnchor.constraint(equalToConstant: 51),
+            
+            guideLabel.topAnchor.constraint(equalTo: goToHomeButton.bottomAnchor, constant: 20),
+            guideLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor)
+        ])
     }
-    */
 
 }
