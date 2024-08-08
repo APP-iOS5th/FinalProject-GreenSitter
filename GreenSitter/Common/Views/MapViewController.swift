cceea97509e9e6060e64d01ce7402d08e85d70d1
iyungui <rsdbddml@gmail.com>
2024-08-08 13:47:32 +0900

    feat: toast message

--- GreenSitter/Common/Views/MapViewController.swift
+++ GreenSitter/Common/Views/MapViewController.swift
@@ -252,6 +252,101 @@ class APISampleBaseViewController: UIViewController, MapControllerDelegate {
             toastLabel.removeFromSuperview()
         })
     }
+    
+    
+    //MARK: - ToastMessage
+    
+    func showToast(withDuration: Double, delay: Double) {
+        let toastLabelWidth: CGFloat = 380
+        let toastLabelHeight: CGFloat = 80
+        
+        // UIView 생성
+        let toastView = UIView(frame: CGRect(x: (self.view.frame.size.width - toastLabelWidth) / 2, y: 75, width: toastLabelWidth, height: toastLabelHeight))
+        toastView.backgroundColor = UIColor.white
+        toastView.alpha = 1.0
+        toastView.layer.cornerRadius = 25
+        toastView.clipsToBounds = true
+        toastView.layer.borderColor = UIColor.gray.cgColor
+        toastView.layer.borderWidth = 1
+        
+        // 쉐도우 설정
+        toastView.layer.shadowColor = UIColor.gray.cgColor
+        toastView.layer.shadowOpacity = 0.5 // 투명도
+        toastView.layer.shadowOffset = CGSize(width: 4, height: 4) // 그림자 위치
+        toastView.layer.shadowRadius = 10
+        
+        // UIImageView 생성 및 설정
+        let image = UIImageView(image: UIImage(named: "logo7"))
+        image.layer.cornerRadius = 25
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.widthAnchor.constraint(equalToConstant: 50).isActive = true  // 이미지의 크기를 설정.
+        image.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        // UIButton 생성
+        let toastButton = UIButton()
+        toastButton.titleLabel?.font = .systemFont(ofSize: 13)
+        toastButton.setTitle("설정", for: .normal)
+        toastButton.setTitleColor(.white, for: .normal)
+        toastButton.backgroundColor = .gray
+        toastButton.layer.cornerRadius = 4
+        toastButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        toastButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        toastButton.addAction(UIAction { _ in
+            if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                if UIApplication.shared.canOpenURL(appSettings) {
+                    UIApplication.shared.open(appSettings)
+                }
+            }
+        }, for: .touchUpInside)
+        
+        // UILabel 생성 및 설정
+        let labelOne = UILabel()
+        labelOne.text = "위치 권한이 필요한 기능입니다."
+        labelOne.textColor = .black
+        labelOne.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        labelOne.textAlignment = .left
+        labelOne.translatesAutoresizingMaskIntoConstraints = false
+        
+        let labelTwo = UILabel()
+        labelTwo.text = "위치 권한 설정 화면으로 이동합니다."
+        labelTwo.textColor = .black
+        labelTwo.font = UIFont.systemFont(ofSize: 12)
+        labelTwo.textAlignment = .left
+        labelTwo.translatesAutoresizingMaskIntoConstraints = false
+        
+        // StackView 생성 및 설정 (Vertical Stack)
+        let labelStackView = UIStackView(arrangedSubviews: [labelOne, labelTwo])
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .leading
+        labelStackView.spacing = 5
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // StackView 생성 및 설정 (Horizontal Stack)
+        let mainStackView = UIStackView(arrangedSubviews: [image, labelStackView, toastButton])
+        mainStackView.axis = .horizontal
+        mainStackView.alignment = .center
+        mainStackView.spacing = 10
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        toastView.addSubview(mainStackView)
+        
+        // Auto Layout 설정
+        NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: toastView.leadingAnchor, constant: 10),
+            mainStackView.trailingAnchor.constraint(equalTo: toastView.trailingAnchor, constant: -10),
+            mainStackView.topAnchor.constraint(equalTo: toastView.topAnchor, constant: 10),
+            mainStackView.bottomAnchor.constraint(equalTo: toastView.bottomAnchor, constant: -10)
+        ])
+        
+        self.view.addSubview(toastView)
+        UIView.animate(withDuration: withDuration, delay: delay, options: .curveEaseOut, animations: {
+            toastView.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastView.removeFromSuperview()
+        })
+    }
 }
 
 extension APISampleBaseViewController: CLLocationManagerDelegate {
@@ -267,9 +362,12 @@ extension APISampleBaseViewController: CLLocationManagerDelegate {
         case .restricted, .notDetermined:
             print("GPS 권한 설정되지 않음")
             getLocationUsagePermission()
+            showToast(withDuration: 1, delay: 4)
+
         case .denied:
             print("GPS 권한 요청 거부됨")
             getLocationUsagePermission()
+            showToast(withDuration: 1, delay: 4)
         default:
             print("GPS: Default")
         }
@@ -288,4 +386,9 @@ extension APISampleBaseViewController: CLLocationManagerDelegate {
         print("Failed to find user's location: \(error.localizedDescription)")
     }
     
+}
+
+
+#Preview {
+    APISampleBaseViewController()
 }
