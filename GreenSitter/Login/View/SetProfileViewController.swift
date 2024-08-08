6f7e0e6826b6e4dfd26b523db45e22d576f19496
jiyong <wldyd2113@gmail.com>
2024-08-08 20:51:56 +0900

    design: SetProfileViewController

--- GreenSitter/Login/View/SetProfileViewController.swift
+++ GreenSitter/Login/View/SetProfileViewController.swift
@@ -6,37 +6,189 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class SetProfileViewController: UIViewController {
+    let storage = Storage.storage()
     
-    lazy var bodyLabel: UILabel = {
+    lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "testView"
-        label.font = UIFont.boldSystemFont(ofSize: 15)
-
+        label.text = "프로필 정보 입력"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
+    func createImageButton() -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.imageView?.contentMode = .scaleAspectFit // 이미지 버튼 내에 맞춰 표시
+        button.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5) // 이미지에 여백 추가
+        return button
+    }
+    
+    
+    lazy var imageButton1 = createImageButton()
+    lazy var imageButton2 = createImageButton()
+    lazy var imageButton3 = createImageButton()
+    lazy var imageButton4 = createImageButton()
+    lazy var imageButton5 = createImageButton()
+    lazy var imageButton6 = createImageButton()
+    
+    lazy var nickNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame.size.height = 30
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = UIColor(rgbRed: 242, green: 242, blue: 242)
+        textField.placeholder = "닉네임을 입력해주세요"
+        textField.clearsOnBeginEditing = true //편집시 기존텍스트필드값 지우기
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 위치 텍스트 추가
+        let label = UILabel()
+        label.text = "닉네임 "
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .black
+        label.sizeToFit()
+        
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: label.frame.width + 10, height: textField.frame.height))
+        label.frame.origin = CGPoint(x: 10, y: (containerView.frame.height - label.frame.height) / 2)
+        containerView.addSubview(label)
+        
+        textField.leftView = containerView
+        textField.leftViewMode = .always
+        
+        return textField
+    }()
+    
+    lazy var nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("다음", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(rgbRed: 110, green: 190, blue: 70) // RGB 값으로 설정
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(nextTap), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("건너뛰기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(skipTap), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var imageStackView1: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageButton1, imageButton2, imageButton3])
+        stackView.axis = .horizontal
+        stackView.spacing = 10 // 간격 추가
+        stackView.distribution = .fillEqually // 버튼들이 동일한 크기로 분배
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    lazy var imageStackView2: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageButton4, imageButton5, imageButton6])
+        stackView.axis = .horizontal
+        stackView.spacing = 10 // 간격 추가
+        stackView.distribution = .fillEqually // 버튼들이 동일한 크기로 분배
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(bodyLabel)
+        
+        // 서브뷰 추가
+        view.addSubview(titleLabel)
+        view.addSubview(imageStackView1)
+        view.addSubview(imageStackView2)
+        view.addSubview(nickNameTextField)
+        view.addSubview(nextButton)
+        view.addSubview(skipButton)
+        
+        setupImage()
+
+        // 제약 조건 설정
         NSLayoutConstraint.activate([
-            bodyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
-            bodyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            imageStackView1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageStackView1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            imageStackView1.widthAnchor.constraint(equalToConstant: 320),  
+            imageStackView1.heightAnchor.constraint(equalToConstant: 100),
+            
+            imageStackView2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageStackView2.topAnchor.constraint(equalTo: imageStackView1.bottomAnchor, constant: 20),
+            imageStackView2.widthAnchor.constraint(equalToConstant: 320),
+            imageStackView2.heightAnchor.constraint(equalToConstant: 100),
+            
+            nickNameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nickNameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 80),
+            nickNameTextField.widthAnchor.constraint(equalToConstant: 350),
+            
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            nextButton.widthAnchor.constraint(equalToConstant: 350),
+            nextButton.heightAnchor.constraint(equalToConstant: 45),
+            
+            skipButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            skipButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -70),
         ])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupImage() {
+        let imageUrls = [
+            "gs://greensitter-6dedd.appspot.com/꽃1.png",
+            "gs://greensitter-6dedd.appspot.com/꽃2.png",
+            "gs://greensitter-6dedd.appspot.com/꽃3.png",
+            "gs://greensitter-6dedd.appspot.com/썩은 씨앗4.png",
+            "gs://greensitter-6dedd.appspot.com/썩은씨앗5.png",
+            "gs://greensitter-6dedd.appspot.com/씨앗1.png"
+        ]
+        
+        let buttons = [imageButton1, imageButton2, imageButton3, imageButton4, imageButton5, imageButton6]
+        
+        for (index, url) in imageUrls.enumerated() {
+            if index < buttons.count {
+                downloadImage(from: url) { image in
+                    DispatchQueue.main.async {
+                        buttons[index].setImage(image, for: .normal)
+                    }
+                }
+            }
+        }
     }
-    */
+    
+    //MARK: - Storage에서 이미지 파일 불러오기
+    func downloadImage(from url: String, completion: @escaping (UIImage?) -> Void) {
+        let imageRef = storage.reference(forURL: url)
+        
+        imageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print("Image download error: \(error)")
+                completion(nil)
+            } else if let data = data, let image = UIImage(data: data) {
+                completion(image)
+            }
+        }
+    }
+    
+    @objc func nextTap() {
 
+        }
+
+    @objc func skipTap() {
+//        let setProfileViewController = SetProfileViewController()
+//        navigationController?.pushViewController(setProfileViewController, animated: true)
+    }
 }
+
+
--- GreenSitter/SceneDelegate.swift
+++ GreenSitter/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see application:configurationForConnectingSceneSession instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let loginViewController = LoginViewController()
+        let loginViewController = SetProfileViewController()
         let navigationController = UINavigationController(rootViewController: loginViewController)
         window.rootViewController = navigationController
         self.window = window
