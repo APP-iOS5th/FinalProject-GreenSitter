8752d6036dbd9e8e0691259c2da75f54349f485c
iyungui <rsdbddml@gmail.com>
2024-08-13 09:54:47 +0900

    fix: 빼먹은 코드 추가 in Location

--- GreenSitter/Common/Models/Location.swift
+++ GreenSitter/Common/Models/Location.swift
@@ -15,10 +15,22 @@ struct Location: Codable, Identifiable {
     let createDate: Date
     let updateDate: Date
     
-    let latitude: Double?
-    let longitude: Double?
-    let address: String?
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    
+    init(enabled: Bool, createDate: Date, updateDate: Date, latitude: Double? = nil, longitude: Double? = nil, address: String? = nil) {
+        self.enabled = enabled
+        self.createDate = createDate
+        self.updateDate = updateDate
+        
+        // 기본값 설정: 위도와 경도가 nil일 경우 서울 시청으로 설정
+        self.latitude = latitude ?? 37.566
+        self.longitude = longitude ?? 126.97
+        self.address = address ?? "서울 중구 세종대로"
+    }
 }
+
 
 extension Location {
     static let seoulLocation = Location(enabled: true, createDate: Date(), updateDate: Date(), latitude: 37.566, longitude: 126.97, address: "서울특별시 동작구 상도로")
