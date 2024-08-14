1bc7c70856b97acb88e9fffe42de7b25e995e89c
Ara Cho <ahrazepam@gmail.com>
2024-08-14 13:20:17 +0900

    feat: add TextView

--- GreenSitter/Post/PostDetailViewController.swift
+++ GreenSitter/Post/PostDetailViewController.swift
@@ -99,8 +99,6 @@ class PostDetailViewController: UIViewController {
     
     private let pickerImageView: UIImageView = {
         let image = UIImageView()
-//        image.clipsToBounds = true2
-//        image.layer.cornerRadius = 10
         image.backgroundColor = .lightGray
         image.tintColor = .gray
         image.image = UIImage(systemName: "photo.on.rectangle.fill")
@@ -128,15 +126,16 @@ class PostDetailViewController: UIViewController {
         button.setTitle("채팅하기", for: .normal)
         button.backgroundColor = .complementary
         button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 15
+        button.layer.cornerRadius = 20
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     private let mapLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .labelsSecondary
         label.font = .systemFont(ofSize: 16)
-        label.text = "거래 희망 장소"
+        label.text = "거래 희망 장소를 선택할 수 있어요."
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -149,8 +148,16 @@ class PostDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         setupLayout()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(pickerImageViewTapped))
+        pickerImageView.addGestureRecognizer(tapGesture)
+        pickerImageView.isUserInteractionEnabled = true // 이미지 뷰 상호작용 활성화
+    }
+    
+    @objc private func pickerImageViewTapped() {
+        presentImagePickerController()
     }
     
     private func setupLayout() {
@@ -176,85 +183,105 @@ class PostDetailViewController: UIViewController {
             backButton.widthAnchor.constraint(equalToConstant: 20),
             backButton.heightAnchor.constraint(equalToConstant: 20),
             
-            
             profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             profileImageView.widthAnchor.constraint(equalToConstant: 50),
             profileImageView.heightAnchor.constraint(equalToConstant: 50),
             
-            
             usernameLabel.topAnchor.constraint(equalTo: profileImageView.topAnchor),
             usernameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 8),
-            
             
             userLevelLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 4),
             userLevelLabel.leadingAnchor.constraint(equalTo: usernameLabel.leadingAnchor),
             
-            
             postTimeLabel.topAnchor.constraint(equalTo: userLevelLabel.bottomAnchor, constant: 4),
             postTimeLabel.leadingAnchor.constraint(equalTo: userLevelLabel.leadingAnchor),
-            
             
             statusLabel.bottomAnchor.constraint(equalTo: postTimeLabel.bottomAnchor, constant: 40),
             statusLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             statusLabel.widthAnchor.constraint(equalToConstant: 40),
             statusLabel.heightAnchor.constraint(equalToConstant: 20),
             
-            
             postTitleLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 50),
             postTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             postTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            
             
             deviderLine1.bottomAnchor.constraint(equalTo: postTitleLabel.topAnchor, constant: 35),
             deviderLine1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             deviderLine1.widthAnchor.constraint(equalToConstant: 360),
             deviderLine1.heightAnchor.constraint(equalToConstant: 1),
             
-            
             deviderLine2.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -20),
             deviderLine2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             deviderLine2.widthAnchor.constraint(equalToConstant: 360),
             deviderLine2.heightAnchor.constraint(equalToConstant: 1),
-            
             
             deviderLine3.bottomAnchor.constraint(equalTo: mapLabel.topAnchor, constant: -10),
             deviderLine3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             deviderLine3.widthAnchor.constraint(equalToConstant: 360),
             deviderLine3.heightAnchor.constraint(equalToConstant: 1),
             
-            
             pickerImageView.widthAnchor.constraint(equalToConstant: 100),
             pickerImageView.heightAnchor.constraint(equalToConstant: 100),
             pickerImageView.topAnchor.constraint(equalTo: deviderLine1.bottomAnchor, constant: 20),
             pickerImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-//            pickerImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            
             
             descriptionLabel.bottomAnchor.constraint(equalTo: deviderLine3.topAnchor, constant: -30),
             descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             
-            
-            contactButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            contactButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
             contactButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             contactButton.widthAnchor.constraint(equalToConstant: 100),
             contactButton.heightAnchor.constraint(equalToConstant: 40),
             
-            
             mapLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             mapLabel.bottomAnchor.constraint(equalTo: mapView.topAnchor, constant: -10 ),
-            
             
             mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            mapView.heightAnchor.constraint(equalToConstant: 200),
+            mapView.heightAnchor.constraint(equalToConstant: 200)
         ])
+    }
+}
+
+extension PostDetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func presentImagePickerController() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let alert = UIAlertController(title: "사진 선택", message: "사진을 가져올 곳을 선택하세요.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "카메라", style: .default, handler: { _ in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("카메라 사용 불가")
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "사진 라이브러리", style: .default, handler: { _ in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            pickerImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
 #Preview {
     return UINavigationController(rootViewController: PostDetailViewController())
 }
-
