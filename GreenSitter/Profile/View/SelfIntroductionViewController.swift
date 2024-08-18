731a0b6f715356f9a4e1cc238cae31b0699cc54e
jiyong <wldyd2113@gmail.com>
2024-08-18 16:59:05 +0900

    desigin: 자기소개페이지 뷰 구성

--- GreenSitter/Profile/View/SelfIntroductionViewController.swift
+++ GreenSitter/Profile/View/SelfIntroductionViewController.swift
@@ -8,9 +8,73 @@
 import UIKit
 
 class SelfIntroductionViewController: UIViewController {
+    var user: User?
+    
+    lazy var titleLabel: UILabel = {
+        let label =  UILabel()
+        label.text = "자기소개"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var introductionTextView: UITextView = {
+       let textView = UITextView()
+        textView.layer.cornerRadius = 10
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.borderWidth = 1.0
+        textView.font = UIFont.systemFont(ofSize: 16)
+        textView.text = user?.aboutMe ?? "자기소개를 입력하세요"
+        textView.textColor = .black
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "x.circle.fill"), for: .normal)
+        button.tintColor = UIColor(named: "SeparatorsOpaque")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(closeButtonTap), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var completeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("완료", for: .normal)
+        button.backgroundColor = UIColor(named: "DominentColor")
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(completeButtonTap), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(closeButton)
+        view.addSubview(introductionTextView)
+        view.addSubview(completeButton)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            introductionTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
+            introductionTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            introductionTextView.widthAnchor.constraint(equalToConstant: 350),
+            introductionTextView.heightAnchor.constraint(equalToConstant: 250),
+            
+            
+            completeButton.topAnchor.constraint(equalTo: introductionTextView.bottomAnchor, constant: 30),
+            completeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            completeButton.widthAnchor.constraint(equalToConstant: 350),
+            completeButton.heightAnchor.constraint(equalToConstant: 45)
+        ])
     }
 }
---
+++ GreenSitter/Profile/ViewModels/SelfIntroductionViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  SelfIntroductionViewModel.swift
+//  GreenSitter
+//
+//  Created by 차지용 on 8/18/24.
+//
+
+import UIKit
+
+extension SelfIntroductionViewController {
+    
+    //MARK: - 취소
+    @objc func closeButtonTap() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - 완료
+    @objc func completeButtonTap() {
+        
+    }
+}
