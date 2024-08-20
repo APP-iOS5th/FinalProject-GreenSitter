5c1a5d7e5771d9290ec80b93674e3657e42d1b5c
kyhlsd <kmyghn@gmail.com>
2024-08-20 18:28:02 +0900

    fix: 약속 자세히 보기 화면도 가능하게

--- GreenSitter/Chat/Views/TestViewController.swift
+++ GreenSitter/Chat/Views/TestViewController.swift
@@ -20,25 +20,39 @@ class TestViewController: UIViewController {
         return plusButton
     }()
     
+    private lazy var planDetailButton: UIButton = {
+        let planDetailButton = UIButton()
+        planDetailButton.setTitle("자세히 보기", for: .normal)
+        planDetailButton.backgroundColor = .systemBlue
+        planDetailButton.translatesAutoresizingMaskIntoConstraints = false
+        planDetailButton.addAction(UIAction { [weak self] _ in
+            let plan = Plan(planId: UUID().uuidString, enabled: true, createDate: Date(), updateDate: Date(), planDate: Date(), planPlace: Location.sampleLocation, contract: nil, ownerNotification: true, sitterNotification: true, isAccepted: true)
+            let makePlanViewModel = MakePlanViewModel(date: plan.planDate, planPlace: plan.planPlace, ownerNotification: plan.ownerNotification, sitterNotification: plan.sitterNotification, progress: 3, isPlaceSelected: true)
+            self?.present(MakePlanViewController(viewModel: makePlanViewModel), animated: true)
+        }, for: .touchUpInside)
+        return planDetailButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         setupUI()
         
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            if settings.authorizationStatus != .authorized {
-                print("Push notification authorization not granted.")
-            } else {
-                DispatchQueue.main.async {
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            }
-        }
+//        UNUserNotificationCenter.current().getNotificationSettings { settings in
+//            if settings.authorizationStatus != .authorized {
+//                print("Push notification authorization not granted.")
+//            } else {
+//                DispatchQueue.main.async {
+//                    UIApplication.shared.registerForRemoteNotifications()
+//                }
+//            }
+//        }
     }
     
     private func setupUI() {
         view.addSubview(plusButton)
+        view.addSubview(planDetailButton)
         
         let safeArea = view.safeAreaLayoutGuide
         
@@ -46,7 +60,12 @@ class TestViewController: UIViewController {
             plusButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -100),
             plusButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 100),
             plusButton.widthAnchor.constraint(equalToConstant: 200),
-            plusButton.heightAnchor.constraint(equalToConstant: 100)
+            plusButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            planDetailButton.bottomAnchor.constraint(equalTo: plusButton.topAnchor, constant: -100),
+            planDetailButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 100),
+            planDetailButton.widthAnchor.constraint(equalToConstant: 200),
+            planDetailButton.heightAnchor.constraint(equalToConstant: 100)
         ])
     }
     
