c017cfeea0af9e4ec6ae56fa5961904d2127b4f7
Jihye Park <deviloper.park@gmail.com>
2024-08-30 13:06:08 +0900

    feat: 실시간 읽음처리 구현

--- GreenSitter/Post/Views/MainPostListViewController.swift
+++ GreenSitter/Post/Views/MainPostListViewController.swift
@@ -273,8 +273,6 @@ extension MainPostListViewController: UITableViewDataSource, UITableViewDelegate
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = viewModel.filteredPosts[indexPath.row]
-        let postDetailViewModel = PostDetailViewModel()
-        postDetailViewModel.selectedPost = post
         let postDetailViewController = PostDetailViewController(postId: post.id)
         postDetailViewController.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(postDetailViewController, animated: true)
