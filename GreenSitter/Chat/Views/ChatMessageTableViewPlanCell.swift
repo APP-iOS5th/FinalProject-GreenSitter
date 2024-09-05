0f2f76695468640d523060401e5bd16556ebb1b2
kyhlsd <kmyghn@gmail.com>
2024-09-05 09:21:52 +0900

    fix: image, plan cell date 들어오도록

--- GreenSitter/Chat/Views/ChatMessageTableViewPlanCell.swift
+++ GreenSitter/Chat/Views/ChatMessageTableViewPlanCell.swift
@@ -286,7 +286,7 @@ class ChatMessageTableViewPlanCell: UITableViewCell {
     }
     
     // MARK: - Cell 구성
-    func configure() {
+    func configure(date: Date) {
         // 프로필 이미지 설정
         let profileImage: String?
         if chatRoom?.userId == chatViewModel?.user?.id {
@@ -307,7 +307,6 @@ class ChatMessageTableViewPlanCell: UITableViewCell {
         profileImageView.kf.setImage(with: profileImageUrl, placeholder: placeholderImage)
         
         // 메세지 시간 설정
-        let date = Date()
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
